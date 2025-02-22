7721aab97aea8f113aefefcbd95856c7a75e0fbd
Abhishek Gawde <abhishekgawde098@gmail.com>
2025-02-22 23:13:55 +0530

    Apply the locomotive to the portfolio

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+function locomotive() {
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+  lerp: 0.05
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+
+locomotive();
+
 let tl = gsap.timeline();
 let main = document.querySelector(".main");
 let cursor = document.querySelector(".cursor");
@@ -332,12 +368,12 @@ function loaderAnimation() {
   tl.to(".loader", {
     opacity: 0,
     duration: 0.2,
-    delay: 2,
+    delay: 1.5,
   });
 
   tl.from(".home-page", {
     y: 5,
-    delay: 0.3,
+    delay: 0.1,
     duration: 2,
     opacity: 0,
     ease: "sine.out",
@@ -357,7 +393,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".scroller",
-      scroller: "body",
+      scroller: ".main",
       start: "top 80%"
     }
   });
@@ -368,7 +404,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".projects h1",
-      scroller: "body",
+      scroller: ".main",
       start: "top 70%"
     }
   });
@@ -379,7 +415,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".project-box1",
-      scroller: "body",
+      scroller: ".main",
       start: "top 70%"
     }
   });
@@ -390,7 +426,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".description1",
-      scroller: "body",
+      scroller: ".main",
       start: "top 70%"
     }
   });
@@ -401,7 +437,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".project-box3",
-      scroller: "body",
+      scroller: ".main",
       start: "top 70%"
     }
   });
@@ -412,7 +448,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".description2",
-      scroller: "body",
+      scroller: ".main",
       start: "top 70%"
     }
   });
@@ -423,7 +459,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".skills h1",
-      scroller: "body",
+      scroller: ".main",
       start: "top 75%"
     }
   });
@@ -435,7 +471,7 @@ function scrollTrigger() {
     stagger: 0.3,
     scrollTrigger: {
       trigger: ".skills-box",
-      scroller: "body",
+      scroller: ".main",
       start: "top 75%"
     }
   });
@@ -446,7 +482,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".about-me h1",
-      scroller: "body",
+      scroller: ".main",
       start: "top 75%"
     }
   });
@@ -457,7 +493,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".left-box",
-      scroller: "body",
+      scroller: ".main",
       start: "top 70%"
     }
   });
@@ -468,7 +504,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".right-top",
-      scroller: "body",
+      scroller: ".main",
       start: "top 70%"
     }
   });
@@ -479,7 +515,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".right-bottom",
-      scroller: "body",
+      scroller: ".main",
       start: "top 95%"
     }
   });
@@ -490,7 +526,7 @@ function scrollTrigger() {
     opacity: 0,
     scrollTrigger: {
       trigger: ".about-box2-bottom",
-      scroller: "body",
+      scroller: ".main",
       start: "top 75%"
     }
   });
